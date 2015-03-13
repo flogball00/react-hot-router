23061cb6ee5fbf1d0ad5d422bad9a2e34dc38db9
Mike Leong <mleong28@gmail.com>
2015-03-13 15:36:04 -0600

    initial

---
+++ scripts/config/routes.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var Router = require('react-router');
+var Route = Router.Route;
+var App = require('../components/App');
+
+
+
+
+var routes = (
+    <Route handler={App} path="/">
+
+    </Route>
+);
+
+
+
+module.exports = routes;
